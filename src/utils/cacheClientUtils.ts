f4356fe9e4e12bfd1028468989e3db25a70cfcf4
elonyavist <elonyavist@gmail.com>
2023-02-06 06:55:53 +0100

    fix: change return type

--- src/utils/cacheClientUtils.ts
+++ src/utils/cacheClientUtils.ts
@@ -113,4 +113,4 @@ export const createInMemoryCacheClientWithNamespace = <T>(namespace: string) =>
 
 export const getResultsFromCache = async (key: string): Promise<CachedResult> => getCacheStrategyInstance().getValueFromCache(CacheNamespaces.RESULTS_NAMESPACE, key);
 
-export const refreshTtlForCachedResult = async <T>(key: string, ttl: number, value: CachedResult<T>): Promise<CachedResult> => getCacheStrategyInstance().refreshTtlForCachedResult(CacheNamespaces.RESULTS_NAMESPACE, key, ttl, value);
+export const refreshTtlForCachedResult = async <T>(key: string, ttl: number, value: CachedResult<T>): Promise<void> => getCacheStrategyInstance().refreshTtlForCachedResult(CacheNamespaces.RESULTS_NAMESPACE, key, ttl, value);
