76cefef693d34da4e682246ce61ef631ba54ac42
Arkadiusz Gil <gil.arkadiusz@yahoo.com>
2024-02-29 07:27:15 +0100

    73 iscached issue (#74)
    * fix(isCached): fixed bad binding of isCache method for mongoose query
    
    * fix(isCached): fixed bad binding of isCache method for mongoose query

--- src/extendAggregate.ts
+++ src/extendAggregate.ts
@@ -15,11 +15,11 @@ export const addCachingToAggregate = (mongoose: Mongoose): void => {
     /**
      * Function to check if given query is cached.
      */
-    mongoose.Query.prototype.isCached = function <T>(context: SpeedGooseCacheOperationParams = {}): Promise<boolean> {
-      prepareAggregateOperationParams(this, context);
+    mongoose.Aggregate.prototype.isCached = function <T>(context: SpeedGooseCacheOperationParams = {}): Promise<boolean> {
+        prepareAggregateOperationParams(this, context);
 
-      return isCached(context.cacheKey)
-  };
+        return isCached(context.cacheKey);
+    };
 };
 
 const execAggregationWithCache = async <R>(aggregation: Aggregate<R[], R>, context: SpeedGooseCacheOperationContext): Promise<Aggregate<R[], R>> => {
