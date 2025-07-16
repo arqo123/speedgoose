6c51368bc203d91a5eac572b9b51234bf5baa84b
Arkadiusz Gil <=gil.arkadiusz@yahoo.com>
2025-07-16 21:08:42 +0200

    fix: update pipeline configuration and add release workflow

--- .releaserc.js
+++ .releaserc.js
@@ -1,18 +1,32 @@
 module.exports = {
-  branches: ['master'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md',
-    }],
-    '@semantic-release/npm',
-    ['@semantic-release/github', {
-        assets: [
-          ['package.json', 'yarn.lock', 'CHANGELOG.md'],
-        ],
-        failTitle: false,
-        failComment: false,
-    }],
+  "branches": [
+    "master",
+    {
+      "name": "next",
+      "prerelease": true
+    }
   ],
-};
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "package.json",
+          "package-lock.json",
+          "CHANGELOG.md"
+        ],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+}
