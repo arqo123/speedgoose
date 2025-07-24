02675a331c547d595c87d80dea376f5fc67b3005
Arkadiusz Gil <=gil.arkadiusz@yahoo.com>
2025-07-24 18:55:15 +0200

    fix(release): update commit-analyzer configuration for improved release rules

--- .releaserc.js
+++ .releaserc.js
@@ -7,7 +7,18 @@ module.exports = {
     }
   ],
   "plugins": [
-    "@semantic-release/commit-analyzer",
+    ["@semantic-release/commit-analyzer", {
+      "preset": "conventionalcommits",
+      "releaseRules": [
+        {"type": "build", "scope": "deps", "release": "patch"},
+        {"type": "fix", "scope": "deps", "release": "patch"},
+        {"type": "build", "release": "patch"},
+        {"type": "fix", "release": "patch"}
+      ],
+      "parserOpts": {
+        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+      }
+    }],
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
